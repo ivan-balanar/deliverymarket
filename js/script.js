e6606993c2ea9cd1dd44029ac99889f5b72c84d5
ivan_bal <semenov.siberian@gmail.com>
2020-07-12 23:32:15 +0700

    add row 'phone' + js files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+// Mobile menu
+
+$(document).ready(function () {
+    var link = $('.mobile-navigation__burger-link');
+    var link_active = $('.mobile-navigation__burger-link');
+    var navigation = $('.mobile-navigation__nav');
+    var nav_active = $('.nav_active');
+
+    link.click(function(){
+        link.toggleClass('.mobile-navigation__burger-link_active');
+        navigation.toggleClass('nav_active')                
+    });
+
+    link_active.click(function(){
+        navigation_active.removeClass('nav_active');
+    });
+});
